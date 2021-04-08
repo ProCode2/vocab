9cb1b7cf770966bdd56b970f791c5e8e214553f2
ProCode2 <bpro249@gmail.com>
2021-04-08 18:48:29 +0530

    deploy to heroku

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// serve the react app
+app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+// redirect unhandled routes to react app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
